b660a819178fbd4d4107f518b37cc43fcf926426
Muhsin Azmal <muhsinstudent12@gmail.com>
2024-10-26 10:59:26 +0600

    feat: added theme switcher and dark light bg added to body

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import ThemeSwitcher from './components/ThemeSwitcher'
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
+    <>
+    <h1 className="text-3xl font-bold underline dark:text-white">
       Hello world!
     </h1>
+    <ThemeSwitcher />
+    </>
   )
 }
 
