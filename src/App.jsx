6b79487416128f5950cd87d1d1ef667b71d0904a
Muhsin Azmal <muhsinstudent12@gmail.com>
2024-11-22 10:28:49 +0600

    lzgit - 2024-11-22T04:28:49.259Z

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,45 @@
-import { Route, Routes } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css'
 import { Muhsin, About, Projects, Contact, Blog } from './pages/';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Muhsin />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/projects",
+    element: <Projects />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  {
+    path: "/blog",
+    element: <Blog />,
+  },
+  {
+    path: "*",
+    element: "404",
+  },
+], {
+  future: {
+    v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true,
+    v7_relativeSplatPath: true,
+    v7_skipActionErrorRevalidation: true,
+    v7_startTransition: true,
+  },
+});
+
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Muhsin />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="*" element={'404'} />
-      </Routes>
-
-
-      {/* <h1>Theme = {theme}</h1>
-      <button type="button" className='bg-blue-500 hover:bg-blue-700 text-white dark:bg-customGray-800 dark:hover:bg-customGray-700 font-bold py-2 px-4 rounded' onClick={handleTheme}>Switch to {theme === 'light' ? 'dark' : 'light'} Theme</button> */}
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App
--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -1,10 +1,12 @@
-import { Helmet } from "react-helmet";
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import PropTypes from 'prop-types';
 export function PageTitle({ title }) {
     return (
-        <Helmet>
-            <title>{`Muhsin Azmal${title ? ` - ${title}` : ''}`}</title>
-         </Helmet>
+        <HelmetProvider>
+            <Helmet>
+                <title>{`Muhsin Azmal${title ? ` - ${title}` : ''}`}</title>
+             </Helmet>
+        </HelmetProvider>
     )
 }
 
